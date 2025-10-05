969ceee1264564f871804966243473a5e5ab8519
Gonzalo Agu <gonzalo.ag88@gmail.com>
2025-10-05 14:40:53 -0400

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { type Request, type Response } from "express";
 import router from "./routes.js";
+import cors from "cors";
+
 
 console.log('Initializing server...');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json());
 app.use(router);
 
