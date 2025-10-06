fb08f264f8d3150bff728263936790c1982628f5
Kevin <kevindana99@gmail.com>
2025-10-05 23:41:49 -0300

    saved api

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import express, { type Request, type Response } from "express";
 import router from "./routes.js";
 import cors from "cors";
 
-
-console.log('Initializing server...');
+console.log("Initializing server...");
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors({
-    origin: '*'
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(router);
-
 
 app.listen(PORT);
 console.log("Server listening at port ", PORT);
